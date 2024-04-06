fcebf0ff0167d9b047f223a67543b07e4ae19fa9
Vlad <89295404+Virashu@users.noreply.github.com>
2024-04-06 20:10:55 +0300

    Update script

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/env bash
 
+# Make venv
 python -m venv .venv
 source .venv/bin/activate
 
+# Install dependencies
 python -m pip install -r requirements.txt
 
+# Install and run this project
 python -m pip install .
 python -m y
